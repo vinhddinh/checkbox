eb206f37e97e57f591a1dd4e2e3cc5de999cb1d2
vinhddinh <vinh@dinh.cc>
2022-10-21 03:55:01 +1100

    Added authentication backend

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from "react-bootstrap";
 
 export default function Home() {
+  const { data: session, status } = useSession();
   return (
     <div className={styles.container}>
       <Head>
@@ -16,8 +19,10 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <Button onClick={() => signIn("github")}>Sign in with GitHub</Button>
+
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +63,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
