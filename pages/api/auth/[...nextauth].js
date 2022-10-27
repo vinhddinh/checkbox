7ffb9b7084306f949af777e964240a0adff5b2fd
vinhddinh <vinh@dinh.cc>
2022-10-28 05:53:50 +1100

    add logged in user to database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,4 +8,28 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      console.log(`signIn: ${user.email}`);
+      // create user if not exists
+      const res = await fetch(
+        `${process.env.NEXTAUTH_URL}/api/users/${user.email}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: user.name,
+          }),
+        }
+      );
+      if (res.status === 201) {
+        console.log(`Created user ${user.email}`);
+      } else if (res.status === 409) {
+        console.log(`User ${user.email} already exists`);
+      }
+      return true;
+    },
+  },
 });
