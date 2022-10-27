8ece53e0da70977e263a371128b7a1191d3fa045
vinhddinh <vinh@dinh.cc>
2022-10-28 00:55:48 +1100

    user endpoint, no auth

---
+++ pages/api/users/[email].js
@@ -0,0 +1,62 @@
+import prisma from "/prisma/client";
+
+export default async function handle(req, res) {
+  switch (req.method) {
+    case "GET":
+      try {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: req.query.email,
+          },
+        });
+        res.status(200).json(user);
+      } catch (e) {
+        res.status(500).json({ error: e });
+      }
+      break;
+    case "POST":
+      try {
+        const user = await prisma.user.create({
+          data: {
+            name: req.body.name,
+            email: req.query.email,
+          },
+        });
+        res.status(201).json(user);
+      } catch (e) {
+        res.status(500).json({ error: e });
+      }
+      break;
+    case "PUT":
+      console.log(req.body);
+      try {
+        const user = await prisma.user.update({
+          where: {
+            email: req.query.email,
+          },
+          data: {
+            name: req.body.name,
+          },
+        });
+        res.status(204);
+      } catch (e) {
+        res.status(500).json({ error: e });
+      }
+      break;
+    case "DELETE":
+      try {
+        const user = await prisma.user.delete({
+          where: {
+            email: req.query.email,
+          },
+        });
+        res.status(200).json(user);
+      } catch (e) {
+        res.status(500).json({ error: e });
+      }
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
