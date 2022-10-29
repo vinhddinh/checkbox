669e41070f1e5ad60920adaeb778e71f4aaad89d
vinhddinh <vinh@dinh.cc>
2022-10-29 20:32:28 +1100

    added inline edit component

---
+++ components/InlineEdit.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export const InlineEdit = ({ value, setValue, strikeThrough }) => {
+  const [editingValue, setEditingValue] = useState(value);
+
+  const onChange = (event) => setEditingValue(event.target.value);
+
+  const onKeyDown = (event) => {
+    if (event.key === "Enter" || event.key === "Escape") {
+      event.target.blur();
+    }
+  };
+
+  const onBlur = (event) => {
+    if (event.target.value.trim() === "") {
+      setEditingValue(value);
+    } else {
+      setValue(event.target.value);
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      value={editingValue}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+      onBlur={onBlur}
+      className={strikeThrough ? "text-decoration-line-through" : ""}
+    />
+  );
+};
