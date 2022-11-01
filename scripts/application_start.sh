275b51eda1eb490c99537c19afa2ebc4a2eb0192
vinhddinh <vinh@dinh.cc>
2022-11-01 19:36:43 +1100

    fixed directory in application start script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,7 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-cd /home/ec2-user/next-app
+cd /home/ec2-user/checkbox
 
 #install node modules
 yarn install --frozen-lockfile || true
